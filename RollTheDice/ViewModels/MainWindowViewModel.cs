97a6c5c78d281deec92a80438dd6e66a120e9708
Michael Kensy <michael.kensy@outlook.com>
2016-10-06 17:30:17 +0200

    Keybindings: Ctrl + N => Add Player Ctrl + D => Remove Player Ctrl + R => Roll

--- RollTheDice/ViewModels/MainWindowViewModel.cs
+++ RollTheDice/ViewModels/MainWindowViewModel.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Threading;
 using RollTheDice.Utilities;
 
 namespace RollTheDice.ViewModels
@@ -17,11 +20,13 @@ namespace RollTheDice.ViewModels
 
             AddNewPlayerCommand = new RelayCommand<object>(action => AddNewPlayer(), canExecute => CanAddNewPlayer());
             RemovePlayerCommand = new RelayCommand<object>(action => RemovePlayer(), canExecute => CanRemovePlayer());
+            RollDiceCommand = new RelayCommand<object>(action => RollDice());
         }
 
         public ObservableCollection<PlayerViewModel> Players { get; private set; }
         public ICommand AddNewPlayerCommand { get; private set; }
         public ICommand RemovePlayerCommand { get; private set; }
+        public ICommand RollDiceCommand { get; private set; }
 
         public PlayerViewModel SelectedPlayer
         {
@@ -55,6 +60,112 @@ namespace RollTheDice.ViewModels
         private void RemovePlayer()
         {
             Players.Remove(this.SelectedPlayer);
+        }
+
+        private void RollDice()
+        {
+            Random rnd = new Random();
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Dispatcher.CurrentDispatcher.Invoke((Action)(() => SetDice(rnd.Next(1, 7))));
+                    Thread.Sleep(100 + 10 * i);
+                }
+            });
+
+
+        }
+
+        public bool IsOneOneVisible { get; set; }
+        public bool IsThreeOneVisible { get; set; }
+        public bool IsOneTwoVisible { get; set; }
+        public bool IsTwoTwoVisible { get; set; }
+        public bool IsThreeTwoVisible { get; set; }
+        public bool IsOneThreeVisible { get; set; }
+        public bool IsThreeThreeVisible { get; set; }
+
+        private void SetDice(int rollResult)
+        {
+            try
+            {
+                switch (rollResult)
+                {
+                    case 1:
+                        IsOneOneVisible = false;
+                        IsThreeOneVisible = false;
+                        IsOneTwoVisible = false;
+                        IsTwoTwoVisible = true;
+                        IsThreeTwoVisible = false;
+                        IsOneThreeVisible = false;
+                        IsThreeThreeVisible = false;
+                        break;
+                    case 2:
+                        IsOneOneVisible = true;
+                        IsThreeOneVisible = false;
+                        IsOneTwoVisible = false;
+                        IsTwoTwoVisible = false;
+                        IsThreeTwoVisible = false;
+                        IsOneThreeVisible = false;
+                        IsThreeThreeVisible = true;
+                        break;
+                    case 3:
+                        IsOneOneVisible = true;
+                        IsThreeOneVisible = false;
+                        IsOneTwoVisible = false;
+                        IsTwoTwoVisible = true;
+                        IsThreeTwoVisible = false;
+                        IsOneThreeVisible = false;
+                        IsThreeThreeVisible = true;
+                        break;
+                    case 4:
+                        IsOneOneVisible = true;
+                        IsThreeOneVisible = true;
+                        IsOneTwoVisible = false;
+                        IsTwoTwoVisible = false;
+                        IsThreeTwoVisible = false;
+                        IsOneThreeVisible = true;
+                        IsThreeThreeVisible = true;
+                        break;
+                    case 5:
+                        IsOneOneVisible = true;
+                        IsThreeOneVisible = true;
+                        IsOneTwoVisible = false;
+                        IsTwoTwoVisible = true;
+                        IsThreeTwoVisible = false;
+                        IsOneThreeVisible = true;
+                        IsThreeThreeVisible = true;
+                        break;
+                    case 6:
+                        IsOneOneVisible = true;
+                        IsThreeOneVisible = true;
+                        IsOneTwoVisible = true;
+                        IsTwoTwoVisible = false;
+                        IsThreeTwoVisible = true;
+                        IsOneThreeVisible = true;
+                        IsThreeThreeVisible = true;
+                        break;
+                    default:
+                        IsOneOneVisible = false;
+                        IsThreeOneVisible = false;
+                        IsOneTwoVisible = false;
+                        IsTwoTwoVisible = false;
+                        IsThreeTwoVisible = false;
+                        IsOneThreeVisible = false;
+                        IsThreeThreeVisible = false;
+                        break;
+                }
+            }
+            finally
+            {
+                OnPropertyChanged("IsOneOneVisible");
+                OnPropertyChanged("IsThreeOneVisible");
+                OnPropertyChanged("IsOneTwoVisible");
+                OnPropertyChanged("IsTwoTwoVisible");
+                OnPropertyChanged("IsThreeTwoVisible");
+                OnPropertyChanged("IsOneThreeVisible");
+                OnPropertyChanged("IsThreeThreeVisible");
+            }
         }
     }
 }
--- RollTheDice/Views/MainWindow.xaml.cs
+++ RollTheDice/Views/MainWindow.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using RollTheDice.ViewModels;
 
-namespace RollTheDice
+namespace RollTheDice.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -26,91 +11,7 @@ namespace RollTheDice
         public MainWindow()
         {
             InitializeComponent();
-            SetDice(7);
             this.DataContext = new MainWindowViewModel();
-        }
-
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            Random rnd = new Random();
-            ThreadPool.QueueUserWorkItem((o) =>
-            {
-                for (int i = 1; i <= 10; i++)
-                {
-                    Dispatcher.Invoke((Action)(() => SetDice(rnd.Next(1, 7))));
-                    Thread.Sleep(100 + 10 * i);
-                }
-            });
-        }
-
-        private void SetDice(int rollResult)
-        {
-            switch (rollResult)
-            {
-                case 1:
-                    OneOne.Visibility = Visibility.Hidden;
-                    ThreeOne.Visibility = Visibility.Hidden;
-                    OneTwo.Visibility = Visibility.Hidden;
-                    TwoTwo.Visibility = Visibility.Visible;
-                    ThreeTwo.Visibility = Visibility.Hidden;
-                    OneThree.Visibility = Visibility.Hidden;
-                    ThreeThree.Visibility = Visibility.Hidden;
-                    break;
-                case 2:
-                    OneOne.Visibility = Visibility.Visible;
-                    ThreeOne.Visibility = Visibility.Hidden;
-                    OneTwo.Visibility = Visibility.Hidden;
-                    TwoTwo.Visibility = Visibility.Hidden;
-                    OneThree.Visibility = Visibility.Hidden;
-                    ThreeTwo.Visibility = Visibility.Hidden;
-                    ThreeThree.Visibility = Visibility.Visible;
-                    break;
-                case 3:
-                    OneOne.Visibility = Visibility.Visible;
-                    ThreeOne.Visibility = Visibility.Hidden;
-                    OneTwo.Visibility = Visibility.Hidden;
-                    TwoTwo.Visibility = Visibility.Visible;
-                    ThreeTwo.Visibility = Visibility.Hidden;
-                    OneThree.Visibility = Visibility.Hidden;
-                    ThreeThree.Visibility = Visibility.Visible;
-                    break;
-                case 4:
-                    OneOne.Visibility = Visibility.Visible;
-                    ThreeOne.Visibility = Visibility.Visible;
-                    OneTwo.Visibility = Visibility.Hidden;
-                    TwoTwo.Visibility = Visibility.Hidden;
-                    OneThree.Visibility = Visibility.Visible;
-                    ThreeTwo.Visibility = Visibility.Hidden;
-                    ThreeThree.Visibility = Visibility.Visible;
-                    break;
-                case 5:
-                    OneOne.Visibility = Visibility.Visible;
-                    ThreeOne.Visibility = Visibility.Visible;
-                    OneTwo.Visibility = Visibility.Hidden;
-                    TwoTwo.Visibility = Visibility.Visible;
-                    OneThree.Visibility = Visibility.Visible;
-                    ThreeTwo.Visibility = Visibility.Hidden;
-                    ThreeThree.Visibility = Visibility.Visible;
-                    break;
-                case 6:
-                    OneOne.Visibility = Visibility.Visible;
-                    ThreeOne.Visibility = Visibility.Visible;
-                    OneTwo.Visibility = Visibility.Visible;
-                    TwoTwo.Visibility = Visibility.Hidden;
-                    ThreeTwo.Visibility = Visibility.Visible;
-                    OneThree.Visibility = Visibility.Visible;
-                    ThreeThree.Visibility = Visibility.Visible;
-                    break;
-                default:
-                    OneOne.Visibility = Visibility.Hidden;
-                    ThreeOne.Visibility = Visibility.Hidden;
-                    OneTwo.Visibility = Visibility.Hidden;
-                    TwoTwo.Visibility = Visibility.Hidden;
-                    ThreeTwo.Visibility = Visibility.Hidden;
-                    OneThree.Visibility = Visibility.Hidden;
-                    ThreeThree.Visibility = Visibility.Hidden;
-                    break;
-            }
         }
     }
 }
